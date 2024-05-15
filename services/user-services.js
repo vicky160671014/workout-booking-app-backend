1f59392aa0916764340a038067945efff4957085
vicky160671014 <vicky160671014@gmail.com>
2024-05-15 15:26:22 +0800

    feat: add user sign up

--- services/user-services.js
+++ services/user-services.js
@@ -1,6 +1,26 @@
 // Services 負責商業邏輯運算
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
 const userServices = {
-  signUp: (req, cb) => {},
+  signUp: async (req, cb) => {
+    try {
+      const { name, email, password, confirmPassword } = req.body
+      if (!name || !email || !password || !confirmPassword) throw new Error('All fields are required')
+      if (password !== confirmPassword) throw new Error('Passwords do not match!')
+
+      const user = await User.findOne({ where: { email } })
+      if (user) throw new Error('Email already exists!')
+
+      const hash = await bcrypt.hash(password, 10)
+
+      const newUser = await User.create({ name, email, password: hash })
+      cb(null, { user: newUser })
+    } catch (error) {
+      cb(error)
+    }
+  },
   signIn: (req, cb) => {}
 }
 
