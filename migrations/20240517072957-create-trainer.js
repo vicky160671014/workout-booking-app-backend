cae97dc83e529af0bcf744f955cf2c9e087000ec
vicky160671014 <vicky160671014@gmail.com>
2024-05-17 15:50:49 +0800

    feat: add trainer model

---
+++ migrations/20240517072957-create-trainer.js
@@ -0,0 +1,48 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Trainers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      introduction: {
+        type: Sequelize.TEXT
+      },
+      teachingStyle: {
+        type: Sequelize.TEXT
+      },
+      duringTime: {
+        type: Sequelize.STRING
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      appointment: {
+        type: Sequelize.JSON
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Trainers')
+  }
+}
--- models/trainer.js
+++ models/trainer.js
@@ -3,27 +3,30 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Trainer extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      User.hasOne(models.Trainer, { foreignKey: 'userId', as: 'isTrainer' })
+      Trainer.belongsTo(models.User, { foreignKey: 'userId', as: 'isUser' })
     }
   };
-  User.init({
+  Trainer.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
     image: DataTypes.STRING,
-    introduction: DataTypes.TEXT
+    introduction: DataTypes.TEXT,
+    teachingStyle: DataTypes.TEXT,
+    duringTime: DataTypes.STRING,
+    location: DataTypes.STRING,
+    appointment: DataTypes.JSON,
+    userId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
-    tableName: 'Users',
+    modelName: 'Trainer',
+    tableName: 'Trainers',
     underscored: true
   })
-  return User
+  return Trainer
 }
